a4e39a270509749c50911a04dae126a637f8fa53
Chris Park <junkin@berkeley.edu>
2019-01-24 12:35:37 -0800

    completed first part of lab1

---
+++ lab1/hellonumbers.java
@@ -0,0 +1,11 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int x = 0;
+        int y = 0;
+        while (x < 46) {
+            System.out.print(x + " ");
+            y = y + 1;
+            x = x + y;
+        }
+    }
+}
