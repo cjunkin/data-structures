b8a5eaf9711565f7a83ee5c8be95e034f54d0fe2
Chris Park <junkin@berkeley.edu>
2019-03-10 21:18:57 -0700

    hw3

--- hw3/hw3/hash/SimpleOomage.java
+++ hw3/hw3/hash/SimpleOomage.java
@@ -26,7 +26,7 @@ public class SimpleOomage implements Oomage {
         if (!USE_PERFECT_HASH) {
             return red + green + blue;
         } else {
-            return (int) Math.round((2.34 * red) % 6 + (4.12 * green) % 7 + (3.57 * blue) % 13);
+            return (int) Math.round((2.34 * red) % 6 + (40.12 * green) % 7 + (3049.57 * blue));
         }
     }
 
